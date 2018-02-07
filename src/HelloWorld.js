1ab8ecf556614660b551e32ad31d57ccc16bc057
Qubeo <32247672+Qubeo@users.noreply.github.com>
2018-02-08 00:45:25 +0100

    HW anim

---
+++ src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+
+    render() {
+        return (
+            <div className="helloContainer">
+                <h1 className="Hello-logo">Hello, world!</h1>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
